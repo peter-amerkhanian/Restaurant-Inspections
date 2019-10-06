9bcf822a6f09545640ee7c6e8717382371e72cad
Peter Amerkhanian <peteamer@gmail.com>
2019-10-06 16:19:11 -0500

    semi-working map

--- js/map.js
+++ js/map.js
@@ -1,17 +1,22 @@
-var map
+var map;
+
+function baseLayer() {
+	var osmUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+	var osmAttrib = 'Map data © <a href="https://openstreetmap.org">OpenStreetMap</a> contributors';
+	return new L.TileLayer(osmUrl, {maxZoom: 12, attribution: osmAttrib });
+}
 
 
 function initmap() {
 	// set up the map
 	map = new L.Map('map');
 	// Bay Area
-    map.setView([37.765205, -122.241638],10);
-
+	map.setView([37.765205, -122.241638], 10);
 	// create the OSM tile
-	var osmUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
-	var osmAttrib = 'Map data © <a href="https://openstreetmap.org">OpenStreetMap</a> contributors';
-	var osm = new L.TileLayer(osmUrl, {minZoom: 8, maxZoom: 12, attribution: osmAttrib});		
-	map.addLayer(osm);
-}
+	var osm = baseLayer();
+	osm.addTo(map);
+	var geojsonLayer = new L.GeoJSON.AJAX("data/restaurant_map.geojson");
+	geojsonLayer.addTo(map);
 
+}
 initmap();
