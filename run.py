df529a2202a3a87608abab5c3baaf33cd01e373b
Peter Amerkhanian <peteamer@gmail.com>
2019-10-09 13:21:48 -0500

    added UI to run.py, updated python reqs

--- run.py
+++ run.py
@@ -1,8 +1,15 @@
 from data_pipeline import to_geojson, to_csv, csv_file
 import os
 
-if os.path.exists(csv_file):
+if os.path.exists("csv"):
     to_geojson()
 else:
     print("You don't have the restaurant data loaded. Do you want to load data now?")
     print("Note: This process can take up to 15 minutes...")
+    answer = input("Y/N: ")
+    if answer.lower() == "y":
+        to_csv()
+        print("CSV created, now creating geojson")
+        to_geojson()
+    else:
+        print("goodbye.")
