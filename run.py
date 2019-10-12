389403ed741185213b36b46ab95079cd1fde589a
Peter Amerkhanian <peteamer@gmail.com>
2019-10-12 14:07:06 -0500

    added to-do

--- run.py
+++ run.py
@@ -1,6 +1,11 @@
 from data_pipeline import to_geojson, to_csv, csv_file
 import os
 
+"""
+TODO: Right now, the incorrect coords supplied by AC are removed.
+These should instead be re-searched by the Open Map API
+"""
+
 if os.path.exists(csv_file):
     to_geojson()
 else:
