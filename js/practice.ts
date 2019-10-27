5df1ed6a91cdcc80855c5742cf8932363c53a1cf
Peter Amerkhanian <peteamer@gmail.com>
2019-10-27 12:53:06 -0500

    updated git ignore

---
+++ js/practice.ts
@@ -0,0 +1,22 @@
+// class Person {
+//     firstName: String;
+//     lastName: String;
+//     dob: Date;
+//     constructor(firstName: string, lastName: string, dob: string) {
+//         this.firstName = firstName;
+//         this.lastName = lastName;
+//         this.dob = new Date(dob);
+//     }
+//     public getFullName() {
+//         return `${this.firstName} ${this.lastName}`;
+//     }
+
+//     public getBirthYear() {
+//         return this.dob.getFullYear();
+//     }
+// }
+
+// const person1: Person = new Person('John', 'Amer', '4-3-1980');
+// const person2: Person = new Person('Mary', 'John', '9-2-1990');
+
+// console.log(person2.getBirthYear());
